b7ab5cc116c300393504f84cd2492b8f8dda9179
Ilir Hamitaj <ilirhamitaj@gmail.com>
2016-07-08 14:22:28 +0200

    added login file

--- www/js/app.js
+++ www/js/app.js
@@ -58,6 +58,14 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       }
     })
+   .state('app.login', {
+      url: '/login',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/login.html',
+        }
+      }
+    })
 
   .state('app.single', {
     url: '/playlists/:playlistId',
