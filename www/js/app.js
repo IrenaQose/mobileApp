9427fabdc7dcd2f16a97e244e461cd09b2fa510e
Irena <Irena>
2016-07-08 15:47:56 +0200

    new module created for search

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'SearchCtrl'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -101,59 +101,4 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       })
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/home');
-})
-.directive('searchBar', [function () {
-	return {
-		scope: {
-			ngModel: '='
-		},
-		require: ['^ionNavBar', '?ngModel'],
-		restrict: 'E',
-		replace: true,
-		template: '<ion-nav-buttons side="right">'+
-						'<div class="searchBar">'+
-							'<div class="searchTxt" ng-show="ngModel.show">'+
-						  		'<div class="bgdiv"></div>'+
-						  		'<div class="bgtxt">'+
-						  			'<input type="text" placeholder="Search" ng-model="ngModel.txt">'+
-						  		'</div>'+
-					  		'</div>'+
-						  	'<i class="icon placeholder-icon" ng-click="ngModel.txt=\'\';ngModel.show=!ngModel.show"></i>'+
-						'</div>'+
-					'</ion-nav-buttons>',
-
-		compile: function (element, attrs) {
-			var icon=attrs.icon
-					|| (ionic.Platform.isAndroid() && 'ion-android-search')
-					|| (ionic.Platform.isIOS()     && 'ion-ios7-search')
-					|| 'ion-search';
-			angular.element(element[0].querySelector('.icon')).addClass(icon);
-
-			return function($scope, $element, $attrs, ctrls) {
-				var navBarCtrl = ctrls[0];
-				$scope.navElement = $attrs.side === 'right' ? navBarCtrl.rightButtonsElement : navBarCtrl.leftButtonsElement;
-
-			};
-		},
-		controller: ['$scope','$ionicNavBarDelegate', function($scope,$ionicNavBarDelegate){
-			var title, definedClass;
-			$scope.$watch('ngModel.show', function(showing, oldVal, scope) {
-				if(showing!==oldVal) {
-					if(showing) {
-						if(!definedClass) {
-							var numicons=$scope.navElement.children().length;
-							angular.element($scope.navElement[0].querySelector('.searchBar')).addClass('numicons'+numicons);
-						}
-
-						title = $ionicNavBarDelegate.getTitle();
-						$ionicNavBarDelegate.setTitle('');
-					} else {
-						$ionicNavBarDelegate.setTitle(title);
-					}
-				} else if (!title) {
-					title = $ionicNavBarDelegate.getTitle();
-				}
-			});
-		}]
-	};
-}])
+});
